cae5f26fcf622470e3904d9fff47e345fb024ebc
Dra-kt <dra_kt@outlook.com>
2024-03-19 20:55:06 +0800

    支持命令行启动GUI

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import sys
-
 from utils.config import config
 from utils.apis import do_request
 from gui import gui_main
 
+
+# 解析命令行参数
+def parse_args():
+    if len(sys.argv) > 1:
+        args = sys.argv[1:]
+        if "-g" in args:
+            gui_main()
+            sys.exit(0)
+
+
 if __name__ == '__main__':
+    parse_args()
     if config.init_getconf() is False or config.username == "":
         gui_main()
         sys.exit(0)
